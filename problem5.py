6a4eb8bad40ede93ddc8369c0e28791b138ff62a
Maddie Witters <mmwitters@gmail.com>
2020-11-18 21:10:00 -0600

    Problem 5

---
+++ problem5.py
@@ -0,0 +1,12 @@
+def smallest_multiple():
+    for i in range(1, 1000000000**2):
+        for j in range(1,21):
+            if i % j == 0:
+                if j == 20:
+                    return i
+                else:
+                    continue
+            else:
+                break
+
+print(smallest_multiple())
