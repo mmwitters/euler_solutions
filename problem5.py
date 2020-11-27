ce737c67c74bd8b056e7544c08beb0c740f87a1e
Maddie Witters <mmwitters@gmail.com>
2020-11-27 17:01:27 -0600

    Problem 5 performance enhancement

--- problem5.py
+++ problem5.py
@@ -1,5 +1,6 @@
 def smallest_multiple():
-    for i in range(1, 1000000000**2):
+    step = 3 * 5 * 7 * 13 * 17 * 19
+    for i in range(step, 1000000000**2, step):
         for j in range(1,21):
             if i % j == 0:
                 if j == 20:
