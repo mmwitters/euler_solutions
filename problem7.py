98382371376be15b7a6a7e731a7d1dc398dd9822
Maddie Witters <mmwitters@gmail.com>
2020-12-02 19:26:38 -0600

    Problem 7

---
+++ problem7.py
@@ -0,0 +1,17 @@
+prime_list = []
+start_num = 2
+
+
+def is_prime(prime_list, number):
+    for prime in prime_list:
+        if number % prime == 0:
+            return False
+    return True
+
+
+while len(prime_list) < 10_002:
+    if is_prime(prime_list, start_num):
+        prime_list.append(start_num)
+    start_num += 1
+
+print(prime_list[10_000])
